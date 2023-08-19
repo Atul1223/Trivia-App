1c8edb32a2378b1b640d96f585cdf85acf375123
Atul1223 <a1atulk@gmail.com>
2023-08-19 14:51:36 +0530

    Repository class
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- app/src/main/java/com/example/triviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/triviaapp/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package com.example.triviaapp.repository
 
+import android.util.Log
 import com.example.triviaapp.data.DataOrException
 import com.example.triviaapp.model.QuestionItem
 import com.example.triviaapp.network.QuestionApi
@@ -8,10 +9,27 @@ import javax.inject.Inject
 class QuestionRepository @Inject constructor(
     private val api: QuestionApi) {
 
-    private val listOfQuestions =
+    private val dataOrException =
         DataOrException<ArrayList<QuestionItem>,
                 Boolean,
                 Exception>()
 
+    suspend fun getAllQuestions() : DataOrException<ArrayList<QuestionItem>,Boolean, Exception> {
+        try {
+
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+
+            if(dataOrException.data.toString().isNotEmpty()){
+                dataOrException.loading = false
+            }
+
+        }catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exception","getAllQuestions(): ${dataOrException.e!!.localizedMessage}")
+        }
+        
+        return dataOrException
+    }
 
 }
