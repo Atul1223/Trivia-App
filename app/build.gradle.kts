d0ce8fa686ef728fa1ebebca5df0965d2e552e49
Atul1223 <a1atulk@gmail.com>
2023-08-19 14:21:02 +0530

    Added Hilt classes and Retrofit
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
 
 kapt {
